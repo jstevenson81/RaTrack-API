efa03553f14a435cc2585b55ce18820ed7336939
Jonathan Stevenson <jonathan@greatfigure8.com>
2019-12-01 21:01:14 -0600

    Added Delete Feelings Function

---
+++ DeleteFeeling/index.ts
@@ -0,0 +1,19 @@
+import { HttpRequest } from '@azure/functions';
+import { Context } from 'vm';
+
+import auth from '../common/auth';
+import auth0Options from '../common/auth/auth0Options';
+import UserLogic from '../common/bus/userLogic';
+
+// get a reference to the auth module
+const auth0 = auth(auth0Options);
+// main trigger
+const httpTrigger = auth0(
+    async (context: Context, req: HttpRequest): Promise<void> => {
+        const logic = new UserLogic();
+        context.res = await logic.getUserAsync(req.params.userName);
+        context.done();
+    }
+);
+
+export default httpTrigger;
